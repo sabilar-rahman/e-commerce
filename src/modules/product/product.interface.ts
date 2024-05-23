256c86076e012bbb5e6fc1b278088a446a72b2ed
sabilar-rahman <sabilarrahman36@gmail.com>
2024-05-23 13:41:09 +0600

    file push

---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,21 @@
+
+type TVariant = {
+    type: string;
+    value: string;
+};
+
+type TInventory = {
+    quantity: number;
+    inStock: boolean;
+};
+
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: [TVariant];
+    inventory: TInventory;
+};
+
